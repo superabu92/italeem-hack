fbd0cf98efdec7220f76c87c3c19f1955074c342
Abu Aiman <superabu92@gmail.com>
2016-08-13 00:23:55 +0800

    Remove Annoying Features of Italeem IIUM:

---
+++ contentscript.js
@@ -0,0 +1,6 @@
+var element = document.getElementById("messagebubble");
+element.parentNode.removeChild(element);
+var element3 = document.getElementsByClassName("dropdown-menu");
+element3[0].parentNode.removeChild(element3[0]);
+var element2 = document.getElementsByClassName("box notice");
+element2[0].parentNode.removeChild(element2[0]);
